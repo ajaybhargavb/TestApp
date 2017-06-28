db5b45f0b0cf79a0e442683344ec0c3d37f8e9a9
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2017-06-28 11:43:49 -0700

    Add project files.

---
+++ WebApplication1/TestTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApplication1
+{
+    public class TestTagHelper : TagHelper
+    {
+        public string Value { get; set; }
+
+        public IDictionary<string, ModelExpression> Dict { get; set; } = new Dictionary<string, ModelExpression>();
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SuppressOutput();
+            output.Content.SetContent($"Processed value: {Value} and {Dict.Count}");
+        }
+    }
+}
